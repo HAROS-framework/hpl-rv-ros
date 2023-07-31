4fe0895ea7e1b0d210de89d9d6ae058255a81e0a
André Santos <git-afsantos@users.noreply.github.com>
2023-07-31 16:56:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,8 +71,9 @@ setup(
     },
     python_requires='>=3.8, <4',
     install_requires=[
-        'hpl-rv<1.0.0',
+        'hpl-rv>=1.0.0',
         'hpl-specs<1.0.0',
+        'Jinja2>=3.1.0',
     ],
     extras_require={
         'dev': ['pytest', 'tox'],
