ad48d83a4ecd24d7b70bfc942efc90eeb8617eca
André Santos <git-afsantos@users.noreply.github.com>
2023-10-04 09:44:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,8 +71,8 @@ setup(
     },
     python_requires='>=3.8, <4',
     install_requires=[
-        'hpl-rv>=1.1.2,<2.0',
-        'hpl-specs>=1.1.2,<2.0',
+        'hpl-rv~=1.1',
+        'hpl-specs~=1.3',
         'Jinja2>=3.1.0',
         'ruamel.yaml>=0.17.0',
     ],
